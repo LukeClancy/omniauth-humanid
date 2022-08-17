cd770589fabbf6ff24f5877f39e890c7206fdf13
Luke Clancy <lukeclancy@hotmail.com>
2022-08-17 16:59:13 -0700

    tweaking it until errors stop happening

--- lib/omniauth-humanid.rb
+++ lib/omniauth-humanid.rb
@@ -1,2 +1,88 @@
-require "omniauth-humanid/version"
-require 'omniauth/strategies/humanid'
+require 'json'
+
+module OmniAuth
+	module Strategies
+		class Humanid
+			include OmniAuth::Strategy
+			#Omniauth strategy creation guide be useful
+			#https://github.com/omniauth/omniauth/wiki/Strategy-Contribution-Guide
+
+			#then this guy created another useful blog 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+			#https://dev.to/vvo/devise-create-a-local-omniauth-strategy-for-slack-4066
+
+			#note the image in the below documentation, I will try to reference back to it below
+			#https://docs.human-id.org/web-sdk-integration-guide#api-request-web-log-in-session
+
+			provider :humanid
+			options :login_button_path, '[LOGIN_BUTTON_PATH]'
+			options :humanid_version, 'v0.0.3'
+			options :local_sign_up_url, "/omniauth/humanid"
+			options :external_signup_url, "https://core.human-id.org/[HUMANID_VERSION]/server/users/web-login"
+			options :lang, :en
+			options :client_secret, nil
+			options :client_id, nil
+
+			def self.humanid_button
+				#see https://docs.human-id.org/web-sdk-integration-guide
+				%Q{<a href="#{options.local_sign_up_url}">
+    				<img src="#{options.login_button_path}" alt="Anonymous Login with humanID" height="27"/>
+				<a>}
+			end
+
+			def get_client_id
+				return options.client_secret unless options.client_secret.nil?
+				raise StandardError.new("Set humanid client id")
+			end
+			def get_client_secret
+				return options.client_secret unless options.client_secret.nil?
+				raise StandardError.new("Set humanid client secret")
+			end
+			def get_external_signup_url
+				url = options.external_signup_url.gsub('[HUMANID_VERSION]', options.humanid_version)
+				url += "?lang=#{options.lang}" if options.lang
+			end
+			def request_phase_err(res)
+				raise StandardError.new("Issue with the request phase of humanid omniauth, response from human id has code: #{res.code}, and body: #{res.body}")
+			end
+			#request phase
+			def request_phase
+				# In the humanid web-sdk-integration-guide, this would be the "[1] login" step. We need to get the redirect url
+				# through a post request, and then send that to the user.
+
+				#see more on Net::HTTP here:
+				#	https://ruby-doc.org/stdlib-2.4.1/libdoc/net/http/rdoc/Net/HTTP.html
+
+				#get uri
+				uri = URI(get_external_signup_url)
+				#make a post request (but dont send it yet)
+				post_request = Net::HTTP::Post.new(uri)
+				#set the headers as per docs.
+				post_request['client-id'] = get_client_id
+				post_request['client-secret'] = get_client_secret
+				post_request['Content-Type'] = 'application/json'
+				#send the request using a weirdly 🤷‍♂️ overcomplicated 🤷‍♂️ method 🤷‍♂️ and 🤷‍♂️ block 🤷‍♂️ blame Net::HTTP
+				res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true){|http| http.request(post_request)}
+
+				if res.code == "200"
+					body = Json.parse(res.body)
+					# they have five diffrent metrics for success:
+					#	1. the response code 200
+					#	2. body["success"] == true
+					#	3. body["code"] == "OK"
+					#	4. body["message"] == "success"
+					#	5. body["data"]["webLoginUrl"] is actually there
+					# I check 1 and 5 since the others seem supplimentary
+					
+					#get the redirect url
+					redirect_url = body.dig("data", "webLoginUrl")
+					#check it, throw an error if nil
+					request_phase_err(res) if redirect_url.nil?
+					#redirect (everything is working!)
+					redirect redirect_url
+				else
+					request_phase_err(res)
+				end
+			end
+		end
+	end
+end
--- lib/omniauth-humanid/version.rb
+++
@@ -1,5 +0,0 @@
-module OmniAuth
-  module Humanid
-    VERSION = "1.0.0"
-  end
-end
